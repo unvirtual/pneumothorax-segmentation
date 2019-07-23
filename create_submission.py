59d318f10fbe1d0a4a8ebf422d30165abefb9db1
Thomas Schutzmeier <tschutzmeier@gmail.com>
2019-07-23 20:55:38 +0000

    create_submission: parallelize mask postprocesing und mask2rle

--- create_submission.py
+++ create_submission.py
@@ -8,14 +8,12 @@ from segmentation_models_pytorch.encoders import get_preprocessing_fn
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 
+from multiprocessing import Pool, cpu_count
+import os
+
 DEV="cpu"
 
-def create_rle(imgs, thr, min_pix, input_dims=(256, 256), output_dims=(1024,1024)):
-    pixels = (imgs > thr/100.).astype(int)
-        
-    if pixels.sum() <= min_pix:
-        return "-1"
-                        
+def extract_mask_clusters(pixels, min_pix, input_dims):
     clusters = measure.label(pixels, neighbors=8, background=0)
     clusters_unique = np.delete(np.unique(clusters), 0)
         
@@ -26,40 +24,76 @@ def create_rle(imgs, thr, min_pix, input_dims=(256, 256), output_dims=(1024,1024
         if m.sum() > min_pix:
             mask += m    
     mask = np.clip(mask, 0,1)
-    
-    if mask.sum() > 1:
-        mask = mask*255
-        mask = cv2.resize(mask[0], (output_dims[0], output_dims[1]))
-        mask = (mask > 128).astype(int)*255
-        # transpose is important!
-        rle = mask2rle(mask.T, output_dims[0], output_dims[1])
-        return rle
-    else:
+    return mask
+
+def filter_mask(pred_masks, thr, min_pix, input_dims=(256, 256)):
+    assert (thr <= 1.0) and (thr >= 0), "Threshold must be in [0,1]"
+    assert min_pix > 0, "min_pix must be > 0"
+    assert pred_masks.max() <= 1.0, "pred_masks must be normalized to [0,1]"
+    assert pred_masks[0].shape == input_dims, "mask shape does not match input dimensions"
+
+    pixels = (pred_masks > thr).astype(int)
+
+    # early exit
+    if pixels.sum() <= min_pix:
+        return None
+    mask = extract_mask_clusters(pixels, min_pix, input_dims)
+
+    if mask.sum() < 1:
+        return None
+
+    return mask
+
+
+def rle_from_predicted_mask(mask, output_dims=(1024,1024)):
+    if mask is None:
         return "-1"
 
-import os
+    assert mask.max() <= 1., "pred mask must be normalized to [0,1]"
+
+    mask = mask*255
+    mask = cv2.resize(mask[0], (output_dims[0], output_dims[1]))
+    mask = (mask > 128).astype(int)*255
+    return mask2rle(mask.T, output_dims[0], output_dims[1])
+
+
+def postprocess_predicted_mask(mask, path, thr, min_pix, input_dims):
+    mask = mask.cpu().numpy()
+    mask = filter_mask(mask, thr, min_pix, input_dims)
+    rle = rle_from_predicted_mask(mask)
+    return [os.path.basename(path)[:-4], rle]
+
+BATCHSIZE=32
 DEV = "cpu"
 def predict_rles(model_file, encoder, pretrained, directory, thr, min_pix, img_size):
     results = []
-    print("Loading Model ...")
+    print("Loading Model", model_file)
     model = torch.load(model_file, map_location=DEV).to(DEV)
     preprocess_input = get_preprocessing_fn(encoder, pretrained=pretrained)
     
-    print("Loading images ...")
+    print("Loading images from", directory)
     df = SIIMDataFrame.from_dirs(directory, labels_file=None).get_siim_dataframe()
     ds = SIIMDataSet(df, img_size, 3, preproc=preprocess_input, has_labels=False)
 
+    pool = Pool(cpu_count())
     print("Loaded %i images" % len(ds))
     model.eval()
 
-    for i, (image, path) in enumerate(ds):
-        print("Prediction %i/%i" % (i+1,len(ds)))
-        image = image.reshape((-1,3,img_size,img_size)).to(DEV)
-        pred = model.predict(image)
-        rle = create_rle(pred.cpu().numpy()[0], thr, min_pix, input_dims=(img_size, img_size))
-        results.extend([[os.path.basename(path)[:-4], rle]])
+    dl = DataLoader(ds,BATCHSIZE)
+    for i, (images, paths) in enumerate(dl):
+        print("Prediction %i/%i" % (i+1,len(dl)))
+
+        images = images.to(DEV)
+        pred = model.predict(images)
+        batch_results = [pool.apply_async(postprocess_predicted_mask, (mask, path, thr, min_pix, (img_size, img_size))) for mask, path in zip(pred, paths)]
+        for r in batch_results:
+            results.extend([r.get()])
+        if i == 4:
+            break
     return results
 
-results = predict_rles("unet_resnet34_input_256_best.pth", "resnet34", "imagenet", "data-original/dicom-images-test", 0.5, 50, 256)
-
-pd.DataFrame(results, columns=["ImageId","EncodedPixels"]).to_csv("submission_unet_resnet34_256_best.csv", index=False)
+if __name__ == "__main__":
+    print("No. of CPUs:", cpu_count())
+    print("batch size: ", BATCHSIZE)
+    results = predict_rles("models/unet_resnet34_input_256_best.pth", "resnet34", "imagenet", "data-original/dicom-images-test", 0.5, 50, 256)
+    pd.DataFrame(results, columns=["ImageId","EncodedPixels"]).to_csv("test_submission.csv", index=False)
