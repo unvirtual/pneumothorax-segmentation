51eb7a882daf9823c4320fb6fca34f825354afc4
Thomas Schutzmeier <tschutzmeier@gmail.com>
2019-08-14 14:05:52 +0200

    make script for spot instances region aware

--- run_dl_training_on_aws_spot.py
+++ run_dl_training_on_aws_spot.py
@@ -1,11 +1,15 @@
+import os
 import boto3
+import json
 import argparse
 import sys
 import base64
+from botocore.exceptions import ClientError
 
 parser = argparse.ArgumentParser()
 parser.add_argument("instance", type=str)
 parser.add_argument("runname", type=str)
+parser.add_argument("region", type=str)
 parser.add_argument("--datascript", type=str, default=None)
 parser.add_argument("-no_training_run", action='store_true')
 parser.add_argument("-no_aws_env_setup", action='store_true')
@@ -13,11 +17,19 @@ args = parser.parse_args()
 
 INSTANCE_TYPE = args.instance
 RUN_NAME = args.runname
+REGION = args.region
 USER_DATA_FILENAME = args.datascript
 NO_TRAINING_RUN = args.no_training_run
 NO_AWS_ENV_SETUP = args.no_aws_env_setup
-AMI_ID = "ami-012e02509f5e9b11f"
 DRY_RUN = False
+
+print("Starting instance in region", REGION)
+
+def key_name(region):
+    return "EC2_KEY_" + region + ".pem"
+
+def security_group_name(region):
+    return "EC2_SSH_SEC_GROUP_" + region
 
 if args.datascript is None and (args.no_training_run or args.no_aws_env_setup):
     print("-no_training_run and -no_aws_env_setup require the --datascript filename option")
@@ -40,8 +52,6 @@ def get_user_data(filename, name, no_training_run, no_aws_env_setup):
         script += file.read()
     return base64.b64encode(script.encode('ascii')).decode('utf-8')
 
-#print(get_user_data(USER_DATA_FILENAME, RUN_NAME, no_aws_env_setup=NO_AWS_ENV_SETUP, no_training_run=NO_TRAINING_RUN))
-
 def abort_on_running_instance(ec2):
     response = ec2.describe_instances()
     for r in response["Reservations"]:
@@ -53,9 +63,111 @@ def abort_on_running_instance(ec2):
                print("Aborting")
                sys.exit()
 
-ec2 = boto3.client('ec2', region_name = "eu-central-1")
+
+def get_ssh_security_group_id(client, region):
+    response = client.describe_security_groups()
+    for sg in response["SecurityGroups"]:
+        if sg["GroupName"] == security_group_name(region):
+            print("SSH SecurityGroup found: %s" % sg["GroupId"])
+            return sg["GroupId"]
+    return None
+
+def create_ssh_security_group(client, region):
+    response = client.describe_vpcs()
+    vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
+
+    try:
+        response = client.create_security_group(GroupName=security_group_name(region),
+                                                Description='SSH Security Group' + region,
+                                                VpcId=vpc_id)
+        security_group_id = response['GroupId']
+        print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
+
+        data = client.authorize_security_group_ingress(
+            GroupId=security_group_id,
+            IpPermissions=[
+                {'IpProtocol': 'tcp',
+                 'FromPort': 22,
+                 'ToPort': 22,
+                 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+            ])
+        print('Ingress Successfully Set %s' % data)
+    except ClientError as e:
+        print(e)
+    return security_group_id
+
+def get_dlami_info(client):
+    SEARCH_STR = "Deep Learning*Ubuntu*24.0"
+    images = client.describe_images(Filters=[{
+        "Name": "name",
+        "Values": [ SEARCH_STR ]
+        }])
+
+    if len(images["Images"]) == 0:
+        print("ERROR: Found no images matching search %s" % SEARCH_STR)
+        return None
+
+    if len(images["Images"]) != 1:
+        print("ERROR: Found more than one image matching search %s" % SEARCH_STR)
+        return None
+
+    image = images["Images"][0]
+    return image
+
+def get_dlami_blockdevice(ami_info):
+    for b in ami_info["BlockDeviceMappings"]:
+        if "Ebs" in b:
+            return b["DeviceName"]
+    return None
+
+def verify_and_setup_keypair(client, region):
+    response = client.describe_key_pairs()
+    key_defined_on_aws = False
+    key_local_path = os.environ["HOME"] + "/.ssh/" + key_name(region)
+    key_local_exists = os.path.exists(key_local_path)
+
+    for k in response["KeyPairs"]:
+        if k["KeyName"] == key_name(region):
+            key_defined_on_aws = True
+            break
+
+    if key_local_exists and not key_defined_on_aws:
+        print("ERROR: Key not defined in AWS but available locally")
+        sys.exit(1)
+
+    if not key_local_exists:
+        if key_defined_on_aws:
+            print("ERROR: Key defined in AWS but not available locally")
+            sys.exit(1)
+        else:
+            print("creating new key pair")
+            response = client.create_key_pair(
+                            KeyName=key_name(region)
+                       )
+            with open(key_local_path, "w") as file:
+                file.write(response["KeyMaterial"])
+
+    print("KeyPair for region %s set up correctly" % REGION)
+
+ec2 = boto3.client('ec2', region_name = REGION)
 print("checking for running instances...")
 abort_on_running_instance(ec2)
+verify_and_setup_keypair(ec2, REGION)
+
+security_group_id = get_ssh_security_group_id(ec2, REGION)
+if security_group_id is None:
+    print("No SSH SecuritGroupy found for region %s" % REGION)
+    security_group_id = create_ssh_security_group(ec2, REGION)
+
+
+dlami_info = get_dlami_info(ec2)
+if dlami_info is None:
+    sys.exit(1)
+print("Using AMI %s (%s)" % (dlami_info["Name"], dlami_info["ImageId"]))
+
+device_name = get_dlami_blockdevice(dlami_info)
+if device_name is None:
+    print("Error: could not determine device for AMI")
 
 response = ec2.request_spot_fleet(
     DryRun=DRY_RUN,
@@ -66,12 +178,12 @@ response = ec2.request_spot_fleet(
             {
                 'SecurityGroups': [
                     {
-                        'GroupId': 'sg-08cd62c1b07a2a7a1'
+                        'GroupId': security_group_id
                     }
                 ],
                 'BlockDeviceMappings': [
                     {
-                        'DeviceName': '/dev/sda1',
+                        'DeviceName': device_name,
                         'Ebs': {
                             'VolumeSize': 90,
                         }
@@ -80,9 +192,9 @@ response = ec2.request_spot_fleet(
                 'IamInstanceProfile': {
                     'Name': 'DL-Training'
                 },
-                'ImageId': AMI_ID,
+                'ImageId': dlami_info["ImageId"],
                 'InstanceType': INSTANCE_TYPE,
-                'KeyName': "AWS_EC2_Key",
+                'KeyName': key_name(REGION),
                 'Monitoring': {
                     'Enabled': False
                 },
