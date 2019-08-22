836c06c95ebc4c101f6ac73df5b8f37202fe0158
Thomas Schutzmeier <tschutzmeier@gmail.com>
2019-08-22 22:06:24 +0200

    try new augmentations

--- augmentations.py
+++ augmentations.py
@@ -6,23 +6,25 @@ def get_augmentations():
     
         album.ShiftScaleRotate(scale_limit=0.2, rotate_limit=10, shift_limit=0.1, p=1, border_mode=0),
     
-        album.IAAAdditiveGaussianNoise(p=0.2),
-        album.IAAPerspective(p=0.2),
+#        album.IAAAdditiveGaussianNoise(p=0.2),
+#        album.IAAPerspective(p=0.2),
     
         album.OneOf(
             [
-                album.CLAHE(p=1),
-                album.RandomBrightness(p=1),
-                album.RandomGamma(p=1)
+#                album.CLAHE(p=1),
+                album.RandomContrast(),
+                album.RandomBrightness(),
+                album.RandomGamma()
             ],
-            p=0.5
+            p=0.3
         ),
         album.OneOf(
             [
-                album.RandomContrast(p=1),
-                album.HueSaturationValue(p=1),
+                album.ElasticTransform(alpha=120, sigma=120*0.05,alpha_affine=120*0.05),
+                album.GridDistortion(),
+                album.OpticalDistortion(distort_limit=2, shift_limit=0.5)
             ],
-            p=0.5,
+            p=0.3,
         )
     ]
     return album.Compose(transforms)
